bc54ffd2d3458f84a70675d89418fd3a87f3db2a
Marco Schwarz <marco.swz@hotmail.com>
2024-08-15 15:03:38 +0200

    Update config for new version

--- machine_config.sh
+++ machine_config.sh
@@ -6,6 +6,5 @@ fi
 
 hyprctl --batch "\
     keyword master:orientation bottom;\
-    keyword workspace 5,monitor:HDMI-A-2,default:true;\
-    keyword workspace 2,monitor:DVI-I-1,default:true;"
+    keyword workspace 5,monitor:HDMI-A-2,default:true;
 
